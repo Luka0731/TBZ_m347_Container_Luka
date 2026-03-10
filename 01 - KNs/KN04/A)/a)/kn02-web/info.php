e6dcc92c91d9af57c09beddb807993880a07ec75
Luka Grujic <luka.grujic@edu.tbz.ch>
2026-03-10 15:54:11 +0100

    kn04 done ppaaawuuu

---
+++ 01 - KNs/KN04/A)/a)/kn02-web/info.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "m347-kn04a-db";
+$username = "root";
+$password = "rootpassword";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT Host, User FROM mysql.user;";
+$result = $conn->query($sql);
+
+echo "<html><body>";
+echo "<p>Diese Seite macht eine Abfrage auf die Datenbank.</p>";
+echo "<p>Das ausgeführte Query ist: <i>" . $sql . "</i></p>";
+echo "<p>Das Resultat:</p><ul>";
+
+while($row = $result->fetch_assoc()) {
+  echo "<li>" . $row["Host"] . " / " . $row["User"] . "</li>";
+}
+
+echo "</ul></body></html>";
+$conn->close();
+?>
